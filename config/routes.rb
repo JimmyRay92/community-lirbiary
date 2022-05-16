e6cd9938bea042f6328f9a786cc709db9245a503
jimmyray92 <jameswang675203@gmail.com>
2022-05-16 20:45:00 +1000

    added user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_scope :user do
     # Redirests signing out users back to sign-in
     get "users", to: "devise/sessions#new"
   end
 
   devise_for :users
+  root 'home#index'
 end
